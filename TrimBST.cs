828c5b223b8dcc9d9be7983422da94f7c0153b2f
Carlos Moraes <carloshrmoraes@gmail.com>
2022-04-15 10:18:30 -0300

    Trim binary search tree

---
+++ TrimBST.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode
+{
+    partial class Solution
+    {
+        public TreeNode TrimBST(TreeNode root, int low, int high)
+        {
+            if (root == null) return null;
+
+            if (root.val > high)
+            {
+                return TrimBST(root.left, low, high);
+            }
+            if (root.val < low)
+            {
+                return TrimBST(root.right, low, high);
+            }
+
+            root.left = TrimBST(root.left, low, high);
+            root.right = TrimBST(root.right, low, high);
+            return root;
+        }
+    }
+}
