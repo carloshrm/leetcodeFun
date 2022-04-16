083fde556ad006b08a8433cbffbd5ec42e8b3844
Carlos Moraes <carloshrmoraes@gmail.com>
2022-04-16 08:50:51 -0300

    convert binary tree

---
+++ ConvertBTS.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode
+{
+    partial class Solution
+    {
+        public int treeSum = 0;
+        public TreeNode ConvertBTS(TreeNode root)
+        {
+            if (root != null)
+            {
+                ConvertBTS(root.right);
+                treeSum += root.val;
+                root.val = treeSum;
+                ConvertBTS(root.left);
+            }
+            return root;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,10 @@ namespace leetcode
         {
             var sln = new Solution();
 
-            var test = sln.SearchBST(tree, 2);
+            var tree = new TreeNode(4);
+            tree.right= new TreeNode(6, new TreeNode(5), new TreeNode(7, null, new TreeNode(8)));
+            tree.left = new TreeNode(1, new TreeNode(0), new TreeNode(2, null, new TreeNode(3)));
+            var test = sln.ConvertBTS(tree);
             //Console.WriteLine(result);
         }
     }
