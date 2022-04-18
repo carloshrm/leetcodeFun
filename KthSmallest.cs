cffdc1cb2abdb5cd3542be573c5a629db75f0250
Carlos Moraes <carloshrmoraes@gmail.com>
2022-04-18 08:21:00 -0300

    smallest val with dfs

---
+++ KthSmallest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode
+{
+    partial class Solution
+    {
+
+        public int KthSmallest(TreeNode root, int k)
+        {
+            var values = new List<int>();
+            dfs(root);
+
+            void dfs(TreeNode r)
+            {
+                if (r == null) return;                
+                dfs(r.left);
+                values.Add(r.val);
+                dfs(r.right);
+            }
+            return values[k - 1];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,9 +9,9 @@ namespace leetcode
             var sln = new Solution();
 
             var tree = new TreeNode(5);
-            tree.right = new TreeNode(6, null, new TreeNode(8, new TreeNode(7), new TreeNode(9)));
+            tree.right = new TreeNode(6);
             tree.left = new TreeNode(3, new TreeNode(2, new TreeNode(1)), new TreeNode(4));
-            var test = sln.IncreasingBST(tree);
+            var test = sln.KthSmallest(tree, 3);
         }
     }
 }
