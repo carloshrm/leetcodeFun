9818edb443ab560883c8573c3cdd0eb7086b52db
Carlos Moraes <carloshrmoraes@gmail.com>
2022-04-19 10:26:26 -0300

    recover wrong order binary tree

--- Program.cs
+++ Program.cs
@@ -8,10 +8,8 @@ namespace leetcode
         {
             var sln = new Solution();
 
-            var tree = new TreeNode(5);
-            tree.right = new TreeNode(6);
-            tree.left = new TreeNode(3, new TreeNode(2, new TreeNode(1)), new TreeNode(4));
-            var test = sln.KthSmallest(tree, 3);
+            var tree = new TreeNode(3, new TreeNode(1), new TreeNode(4, new TreeNode(2), null));
+            sln.RecoverTree(tree);
         }
     }
 }
---
+++ RecoverTree.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode
+{
+    partial class Solution
+    {
+        public void RecoverTree(TreeNode root)
+        {
+            TreeNode firstBroken = null;
+            TreeNode secondBroken = null;
+            TreeNode rearClimber = null;
+
+            checkNodes(root);
+            int valHolder = firstBroken.val;
+            firstBroken.val = secondBroken.val;
+            secondBroken.val = valHolder;
+
+            void checkNodes(TreeNode frontClimber)
+            {
+                if (frontClimber == null) return;
+                checkNodes(frontClimber.left);
+                if (rearClimber != null && rearClimber.val > frontClimber.val)
+                {
+                    if (firstBroken == null)
+                    {
+                        firstBroken = rearClimber;
+                    }
+                    secondBroken = frontClimber;
+                }
+                rearClimber = frontClimber;
+                checkNodes(frontClimber.right);
+            }
+        }
+    }
+}
